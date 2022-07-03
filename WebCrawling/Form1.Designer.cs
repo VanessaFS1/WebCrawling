a0f4a077022cfd79bcd22be6fe756b402fe8627e
Vanessa Freitas <vanessafs1@yahoo.com.br>
2022-07-02 21:32:21 -0300

    Adicionar arquivos de projeto.

---
+++ WebCrawling/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace WebCrawling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AtualizarButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Significados = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sinônimos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Relacionadas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Significados,
+            this.Sinônimos,
+            this.Relacionadas});
+            this.dataGridView1.Location = new System.Drawing.Point(19, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1048, 296);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // AtualizarButton
+            // 
+            this.AtualizarButton.Location = new System.Drawing.Point(359, 12);
+            this.AtualizarButton.Name = "AtualizarButton";
+            this.AtualizarButton.Size = new System.Drawing.Size(95, 23);
+            this.AtualizarButton.TabIndex = 1;
+            this.AtualizarButton.Text = "Pesquisar";
+            this.AtualizarButton.UseVisualStyleBackColor = true;
+            this.AtualizarButton.Click += new System.EventHandler(this.AtualizarButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite a palavra que deseja pequisar: ";
+            // 
+            // Significados
+            // 
+            this.Significados.HeaderText = "Significados";
+            this.Significados.Name = "Significados";
+            this.Significados.Width = 300;
+            // 
+            // Sinônimos
+            // 
+            this.Sinônimos.HeaderText = "Sinônimos";
+            this.Sinônimos.Name = "Sinônimos";
+            this.Sinônimos.Width = 300;
+            // 
+            // Relacionadas
+            // 
+            this.Relacionadas.HeaderText = "Palavras Relacionadas";
+            this.Relacionadas.Name = "Relacionadas";
+            this.Relacionadas.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.AtualizarButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "WebCrawling";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button AtualizarButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Significados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sinônimos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Relacionadas;
+    }
+}
+
---
+++ WebCrawling/Form1.cs
@@ -0,0 +1,97 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebCrawling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void AtualizarButton_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient();
+            string palavraDigitada = textBox1.Text.Replace(" ", "_");
+            string pagina = wc.DownloadString($"https://dicionariocriativo.com.br/{palavraDigitada}");
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(pagina);
+
+            dataGridView1.Rows.Clear();
+
+            string palavra = string.Empty;
+            string significados = string.Empty;
+            //String[] listasignificados = new String[20];
+            List<string> relacionadas = new List<string>();
+            string sinonimos = string.Empty;
+            //string relacionadas = string.Empty;
+
+            var sections = htmlDocument.DocumentNode.Descendants("section").Where(node => node.GetAttributeValue("id", "").Equals("mainContent")).ToList();
+            foreach (var section in sections)
+            {
+                //palavra = section.Descendants("strong").FirstOrDefault().InnerText;
+                ////significados = section.Descendants("section").FirstOrDefault().ChildAttributes("a").FirstOrDefault().Value;
+                //dataGridView1.Rows.Add(palavra);
+
+                var sects = htmlDocument.DocumentNode.Descendants("section").Where(node => node.GetAttributeValue("id", "").Equals("significado")).ToList();
+
+                //int count = 0;
+
+                foreach (var sec in sects)
+                {
+
+                    significados = sec.Descendants("a").FirstOrDefault(x => x.Attributes["class"].Value.Equals("c_primary_hover")).InnerText.Replace("\n", "");
+                    dataGridView1.Rows.Add(significados);
+                    //listasignificados[count] = significados;
+                    //count++;
+                }
+
+                var sectis = htmlDocument.DocumentNode.Descendants("section").Where(node => node.GetAttributeValue("id", "").Equals("sinant")).ToList();
+                foreach (var sec in sectis)
+                {
+                    var lis = sec.Descendants("li").ToList();
+                    foreach (var s in lis)
+                    {
+                        sinonimos = s.Descendants("a").FirstOrDefault(x => x.Attributes["class"].Value.Equals("c_primary_hover")).InnerText.Replace("\n", "");
+                        dataGridView1.Rows.Add("", sinonimos);
+                    }
+                }
+
+                var sect = htmlDocument.DocumentNode.Descendants("section").Where(node => node.GetAttributeValue("id", "").Equals("analogico")).ToList();
+                foreach (var sec in sect)
+                {
+                    //var lis = htmlDocument.DocumentNode.Descendants("ul").Where(node => node.GetAttributeValue("class", "").Equals("tags")).ToList();
+                    //palavra = section.Descendants("strong").FirstOrDefault().InnerText;
+                    var lis = sec.Descendants("li").ToList();
+                    foreach (var l in lis)
+                    {
+                        var rel = l.Descendants("a").FirstOrDefault(x => x.Attributes["class"].Value.Equals("c_primary_hover c_border_hover analogico")).InnerText.Replace("\n", "");
+                        relacionadas.Add(rel);
+                        dataGridView1.Rows.Add("", "", rel);
+                    }
+                    
+                }
+
+                
+
+            }
+
+        }
+    }
+}
